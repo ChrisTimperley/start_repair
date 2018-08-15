5f2bce1cbf992bffe88fe593720c505674defefd
ChrisTimperley <christimperley@googlemail.com>
2018-08-14 21:44:59 -0400

    added helper functions for building CLI options

--- start_repair/cli.py
+++ start_repair/cli.py
@@ -17,39 +17,94 @@ START [Repair]
 """.strip()
 
 
+# type: (str) -> Scenario
+def load_scenario(filename):
+    logger.info("loading scenario from file [%s]", args.filename)
+    scenario = Scenario.from_file(filename)
+    logger.info("loaded scenario [%s] from file", scenario.name)
+    return scenario
+
+
+def add_debug_option(parser):
+    parser.add_argument('--debug',
+                        help="prints debugging information to the stdout.",
+                        action='store_true',
+                        default=False)
+
+
+def add_mission_options(parser):
+    parser.add_argument('--timeout',
+                        help="the number of seconds that may pass without success until a mission is considered a failure.",
+                        type=int,
+                        default=300)
+    parser.add_argument('--timeout-liveness',
+                        help="the number of seconds that may pass without communication with the rover until the mission is aborted.",
+                        type=int,
+                        default=30.0)
+    parser.add_argument('--speedup',
+                        help="the speed-up factor that should be applied to the simulation clock.",
+                        type=float,
+                        default=10.0)
+
+
+def cmd_localize(args):
+    logger.info("performing fault localization for scenario")
+
+    fn_out = "coverage.json"
+
+    logger.info("saved fault localization to disk: %s", fn_out)
+
+
+# FIXME where should we save the patch?
+def cmd_repair(args):
+    scenario = load_scenario(args.filename)
+    logger.info("repairing scenario")
+
+    logger.info("successfully repaired scenario")
+
+
+def cmd_validate(args):
+    scenario = load_scenario(args.filename)
+    logger.info("validating scenario")
+    snapshot = Snapshot.build(filename=scenario,
+                              timeout_mission=args.timeout,
+                              timeout_liveness=args.timeout_liveness,
+                              speedup=args.speedup,
+                              check_waypoints=True,  # FIXME
+                              use_oracle_workaround=False)  # FIXME
+    logger.info("validated scenario")
+
+
+def cmd_analyze(args):
+    logger.info("loading scenario from file [%s]", args.filename)
+    scenario = Scenario.from_file(args.filename)
+    logger.info("loaded scenario [%s] from file", scenario.name)
+    logger.info("performing static analyis of scenario")
+
+    fn_out = "analysis.json"
+
+    logger.info("saved static analysis to disk: %s", fn_out)
+
+
 def main():  # type: () -> None
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     subparsers = parser.add_subparsers()
 
-    # - validate
-    # - localize
-    # - analyze
-    # - repair
-
+    # [validate]
     cmd = subparsers.add_parser('validate',
         help='ensures that a given scenario behaves as expected.')
     cmd.add_argument('filename',
-                     help="the path to the scenario's configuration file.")
-    cmd.add_argument('--timeout',
-                     help="the number of seconds that may pass without success until a mission is considered a failure.",
-                     type=int,
-                     default=300)
-    cmd.add_argument('--timeout-liveness',
-                     help="the number of seconds that may pass without communication with the rover until the mission is aborted.",
-                     type=int,
-                     default=30.0)
-    cmd.add_argument('--speedup',
-                     help="the speed-up factor that should be applied to the simulation clock.",
-                     type=float,
-                     default=10.0)
-    cmd.add_argument('--debug',
-                     help="prints debugging information to the stdout.",
-                     action='store_true',
-                     default=False)
-    def cmd_validate(args):
-        snapshot = Snapshot.build(args.filename, args.timeout, args.speedup)
-        logger.info("validated scenario: %s", snapshot.name)  # FIXME
+                     help="the path to the scenario configuration file.")
+    add_mission_options(cmd)
+    add_debug_option(cmd)
     cmd.set_defaults(func=cmd_validate)
+
+    # [localize]
+
+    # [analyze]
+
+    # [repair]
+
 
     log_to_stdout_formatter = logging.Formatter(
         '%(asctime)s:%(levelname)s: %(message)s',
