76d3d3b8d50fa9e4a32b7fe484eff0f411f9ef43
ChrisTimperley <christimperley@googlemail.com>
2018-08-21 17:05:10 -0400

    implemented basic analysis module

--- start_repair/analyze.py
+++ start_repair/analyze.py
@@ -1,13 +1,23 @@
 from typing import Optional
+import logging
 
 from bugzoo.manager import BugZoo as BugZooClient  # FIXME temporary hack
 from kaskara.analysis import Analysis
 
 from .snapshot import Snapshot
 
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
 
-def analyze(snapshot,           # type: Snapshot
-            client_bugzoo=None  # type: Optional[BugZooClient]
-            ):                  # type: (...) -> Analysis
-    # TODO compute list of files
+
+def analyze(snapshot):  # type: (Snapshot) -> Analysis
+    client_bugzoo = BugZooClient()
+    client_bugzoo.bugs.add(snapshot)
+
+    # FIXME fetch a list of files
+    files = ['/opt/ardupilot/APMrover2/mode.cpp']
+
+    logger.debug("performing analysis of snapshot: %s", snapshot)
     analysis = Analysis.build(client_bugzoo, snapshot, files)
+    logger.debug("performed analysis of snapshot")
+    return analysis
