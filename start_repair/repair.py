8140273053985caafa7b3fd2b0ebb91aaceddd58
ChrisTimperley <christimperley@googlemail.com>
2018-08-23 11:45:56 -0400

    implemented search method

--- start_repair/repair.py
+++ start_repair/repair.py
@@ -1,3 +1,7 @@
+__all__ = ['transformations', 'search']
+
+from datetime import timedelta
+import logging
 
 from bugzoo.core.coverage import TestSuiteCoverage
 from bugzoo.manager import BugZoo
@@ -5,13 +9,18 @@ from start_core.scenario import Scenario
 from darjeeling.localization import Localization
 from darjeeling.problem import Problem
 from darjeeling.snippet import SnippetDatabase
+from darjeeling.candidate import all_single_edit_patches
 from darjeeling.transformation import find_all as find_all_transformations
 from darjeeling.transformation import Transformation, \
                                       InsertStatement
+from darjeeling.searcher import Searcher
 
 from .localize import localize
 from .snapshot import Snapshot
 from .analyze import Analysis
+
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
 
 
 def transformations(snapshot,   # type: Snapshot
@@ -34,20 +43,32 @@ def transformations(snapshot,   # type: Snapshot
     return transformations
 
 
-def repair(snapshot,            # type: Snapshot
-           localization,        # type: Localization
-           coverage,            # type: TestSuiteCoverage
-           analysis,            # type: Analysis
-           transformations,     # type: List[Transformation]
-           threads,             # type: int
-           candidate_limit,     # type: Optional[int]
-           time_limit_mins      # type: Optional[float]
-           ):                   # type: (...) -> None
+def search(snapshot,                # type: Snapshot
+           localization,            # type: Localization
+           coverage,                # type: TestSuiteCoverage
+           analysis,                # type: Analysis
+           transformations,         # type: List[Transformation]
+           threads=1,               # type: int
+           candidate_limit=None,    # type: Optional[int]
+           time_limit_mins=None     # type: Optional[float]
+           ):                       # type: (...) -> None
     client_bugzoo = BugZoo()
     client_bugzoo.bugs.add(snapshot)
-    problem = Problem(bugzoo, snapshot, coverage, analysis=analysis)
+    problem = Problem(client_bugzoo, snapshot, coverage, analysis=analysis)
 
-    time_limit = None
+    if time_limit_mins:
+        logger.debug("time limit for search process: %d minutes",
+                     time_limit_mins)
+        time_limit = timedelta(minutes=time_limit_mins)
+    else:
+        logger.debug("no time limit specified")
+        time_limit = None
+
+    if candidate_limit:
+        logger.debug("candidate limit: %d candidates", candidate_limit)
+    else:
+        logger.debug("no candidate limit specified")
+
     candidates = all_single_edit_patches(transformations)
     search = Searcher(bugzoo=client_bugzoo,
                       problem=problem,
@@ -55,3 +76,4 @@ def repair(snapshot,            # type: Snapshot
                       threads=threads,
                       candidate_limit=candidate_limit,
                       time_limit=time_limit)
+    return search
